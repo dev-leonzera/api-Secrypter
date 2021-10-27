dbd244f8ccb6567ce5c435e3da4dc6a1168ddbbe
Leonardo <leonandrade22@gmail.com>
2021-10-27 10:43:42 -0300

    refactor: string generation process refactored

--- src/modules/generator/service.js
+++ src/modules/generator/service.js
@@ -1,47 +1,40 @@
+const { stringFactory } = require("./utils");
+
 module.exports = {
     create(size) {
         const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
         const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        const symbols = ['!', '@', '#', '$', '%', '&'];        
-        const password = [];
+        const symbols = ['!', '@', '#', '$', '%', '&'];
         var i = 0;
         var found = false;
+        var bag = [];
 
-        while (i <= size) {
-            const randLetters = Math.floor(Math.random() * letters.length)
-            const randNumbers = Math.floor(Math.random() * numbers.length)
-            const randSymbols = Math.floor(Math.random() * symbols.length)
-            //
-            const switchType = Math.floor(Math.random() * 3)
+        for (; i <= size * 2; i++) {
+            var randLetters = Math.floor(Math.random() * letters.length)
+            var randNumbers = Math.floor(Math.random() * numbers.length)
+            var randSymbols = Math.floor(Math.random() * symbols.length)
+            var switchType = Math.floor(Math.random() * 3)
 
             switch (switchType) {
                 case 0:
-                    found = password.includes(letters[randLetters]);
-                    if (!found) {
-                        password.push(letters[randLetters]);
-                    }
+                    bag.push(letters[randLetters])
                     break;
                 case 1:
-                    found = password.includes(numbers[randNumbers]);
-                    if (!found) {
-                        password.push(numbers[randNumbers]);
-                    }
+                    bag.push(numbers[randNumbers]);
                     break;
                 case 2:
-                    found = password.includes(symbols[randSymbols]);
-                    if (!found) {
-                        password.push(symbols[randSymbols]);
-                    }
+                    bag.push(symbols[randSymbols]);
                     break;
 
                 default:
                     break;
             }
-            i++;
         }
-        const temp = password.toString();
-        const finalPass = temp.split(',').join("");
-        return finalPass;
+
+        var password = stringFactory(bag, size);
+
+        return password;
+
     },
     createReminder() { },
     listReminder() { },
--- src/modules/generator/utils.js
+++ src/modules/generator/utils.js
@@ -1,14 +1,14 @@
-module.exports = {
-    randomString(len, an) {
-        an = an && an.toLowerCase();
-        var str = "",
-        i = 0,
-        min = an == "a" ? 10 : 0,
-        max = an == "n" ? 10 : 62;
-        for (; i++ < len;) {
-            var r = Math.random() * (max - min) + min << 0;
-            str += String.fromCharCode(r += r > 9 ? r < 36 ? 55 : 61 : 48);
+module.exports = {    
+    stringFactory(array, len){
+        const stringArray = [];
+        var found = false;
+        for(i=0;i<len;i++){
+            found = stringArray.includes(array[i]);
+            if(!found){
+                stringArray.push(array[i]);
+            }
         }
+        const str = stringArray.toString().split(',').join("")
         return str;
     }
 }
