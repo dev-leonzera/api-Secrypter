986f2bb7880103915d148b8c9245dc32a9cf9b59
Leonardo <leonandrade22@gmail.com>
2021-12-02 16:32:15 -0300

    added .env config

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const cors = require('cors')
 
 const app = express()
 const routes = require('./routes')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 4000;
 
 app.use(cors())
 app.use(express.json());
@@ -10,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes)
 
 const run = () => {
-    app.listen(3333, () => {
+    app.listen(PORT, () => {
         console.log('Conectado')
     })
 }
